aee6ca0df4ea51d5bab650143a503d5bba975748
Mike Njovu <mikemnjovu@gmail.com>
2024-12-27 01:08:23 +0200

    add: side navigation to dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,9 +28,136 @@
             @endisset
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                <div class="flex w-full h-screen ">
+                    <!-- Sidebar -->
+                    <aside class="w-56 shadow-lg min-h-screen  bg-white dark:bg-gray-800">
+                        <div class="p-4 border-b">
+                            <h1 class="text-2xl font-bold dark:text-white text-gray-800">Good Nature</h1>
+                        </div>
+                        <nav class="p-4  bg-white dark:bg-gray-800">
+                            <ul>
+                                <li>
+                                    <a
+                                        href="/dashboard"
+                                        class="flex items-center px-4 py-2 dark:text-white
+                                         text-gray-700 rounded hover:bg-gray-200"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="w-5 h-5 mr-2"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M3 10h18M9 21H3a1 1 0 01-1-1V3a1 1 0 011-1h18a1 1 0 011 1v8"
+                                            />
+                                        </svg>
+                                        Overview
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="/farmers"
+                                        class="flex items-center px-4 py-2 mt-2 dark:text-white text-gray-700 rounded hover:bg-gray-200"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="w-5 h-5 mr-2"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M6 3h12M6 8h12M6 13h6"
+                                            />
+                                        </svg>
+                                        Farmers
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="/loans"
+                                        class="flex items-center px-4 py-2 mt-2 dark:text-white text-gray-700 rounded hover:bg-gray-200"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="w-5 h-5 mr-2"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M8 17l4-4-4-4m4 4H4m14 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                                            />
+                                        </svg>
+                                        Loans
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="/modules"
+                                        class="flex items-center px-4 py-2 mt-2 dark:text-white text-gray-700 rounded hover:bg-gray-200"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="w-5 h-5 mr-2"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M5 11l4-4 4 4m4-4v12a1 1 0 01-1 1H6a1 1 0 01-1-1V7"
+                                            />
+                                        </svg>
+                                        Modules
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="/reports"
+                                        class="flex items-center px-4 py-2 dark:text-white mt-2 text-gray-700 rounded hover:bg-gray-200"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="w-5 h-5 mr-2"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M9 17v-4m0-4v-4m12 8a1 1 0 011 1v6a1 1 0 01-1 1h-6a1 1 0 01-1-1v-6a1 1 0 011-1h6zm-6 4h6"
+                                            />
+                                        </svg>
+                                        Reports
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </aside>
+
+                    <!-- Main Content -->
+                    <main class="flex-1 p-6">
+                        {{ $slot }}
+                    </main>
+                </div>
+
+
         </div>
     </body>
 </html>
