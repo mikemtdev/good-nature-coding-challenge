e696729ea2fe52bf7fee28241c4f81e13b4e2d2f
Mike Njovu <mikemnjovu@gmail.com>
2024-12-27 02:32:49 +0200

    fix(db): add unique property to farm number to ensure no duplicate famer

--- database/migrations/2024_12_25_181745_farmer.php
+++ database/migrations/2024_12_25_181745_farmer.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('farmers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone');
+            $table->string('phone')->unique();
             $table->string('location');
             $table->timestamps();
         });
